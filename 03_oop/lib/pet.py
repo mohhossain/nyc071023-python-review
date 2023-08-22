f08f8677fe98e96f50d35d7e60808a901e4a74f8
Mohammad <moh.hossain98@gmail.com>
2023-08-22 16:21:45 -0400

    solution

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -4,15 +4,32 @@
 # 2. ✅ Instantiate a few pet instance 
     # Compare the pet instances to demonstrate they are not the same object
     # Note: add 'pass' to the pet class 
+    
+    
+    # {
+    #     name: 'Ronil',
+    #     age: 23
+    # }
 
 
 class Pet:
     # pass
-# 3. ✅ Demonstrate __init__ 
+# 3. ✅ Demonstrate __init__   
     # Add arguments to instances  
     # use dot notation to access their attributes 
     # update attributes with new values 
+    def __init__(self, breed, name, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+    
 
+# class Car:
+    
+#     def __init__(self, brand, model, year = 2000):
+#         self.brand = brand
+#         self.year = year
+#         self.model = model
 
 # 4.✅ Demonstrate instance methods by creating a print_pet_details function that will print the pet attributes
 #     Review the self keyword 
@@ -23,8 +40,15 @@ class Pet:
             # breed:domestic longhair
             # temperament:sweet
             # image_url:rose.jpg
+            
+    def print_pet_details(self):
+        print(f'name: {self.name} \nage: {self.age} \nbreed: {self.breed}')
 
+    # def __str__(self):
+    #     return f'The pet name is {self.name}, it is {self.age} year old, and a {self.breed}'
 
+    def __repr__(self):
+        return f'name: {self.name} \nage: {self.age} \nbreed: {self.breed}'
 # Demonstrate instances 
     # Different Instances are Different Objects
 # Demonstrate __init__
@@ -32,6 +56,26 @@ class Pet:
 # Demonstrate the self keyword 
 # Stretch Goals
 # Demonstrate object properties
+
+class Car:
+    
+    def __init__(self, brand, model, year = 2000):
+        self.brand = brand
+        self.year = year
+        self.model = model
+        self.speed = 0
+        
+    def drive(self):
+        self.speed = 50
+        return 'The car is driving'
+    
+    def accelerate(self):
+        self.speed += 10
+        return f'The speed now is {self.speed}mph'
+    
+    def stop(self):
+        self.speed = 0
+        return 'The car stopped!'
 
 # Instances 
 
